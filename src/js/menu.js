b4f6ae1d73def74bcbb35de4ca3601ef1e8cb099
VikMelanchenko <63492650+VikMelanchenko@users.noreply.github.com>
2020-10-13 23:16:53 +0300

    add template string for menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,6 @@
+import cardMenuTmpl from "../templates/card-menu.hbs";
+import cardsMenuTmpl from "../templates/cards-menu.hbs";
+
 import menuItems from "../menu.json";
 
 const menuElements = document.querySelector(".js-menu");
@@ -8,34 +11,5 @@ menuElements.insertAdjacentHTML("beforeend", menuContainer);
 
 // шаблонная строка
 function createMenuItems(menuItems) {
-  return menuItems
-    .map(({ name, description, image, price }) => {
-      return `<li class="menu__item">
-  <article class="card">
-    <img
-      src=${image}
-      alt=${name}
-      class="card__image"
-    />
-    <div class="card__content">
-      <h2 class="card__name">${name}</h2>
-      <p class="card__price">
-        <i class="material-icons"> monetization_on </i>
-        "${price}"
-      </p>
-
-      <p class="card__descr">
-        "${description}"
-      </p>
-
-    </div>
-
-    <button class="card__button button">
-      <i class="material-icons button__icon"> shopping_cart </i>
-      В корзину
-    </button>
-  </article>
-</li>`;
-    })
-    .join("");
+  return cardsMenuTmpl(menuItems);
 }
